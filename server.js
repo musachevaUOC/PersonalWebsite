e972d12f8b90696bc529569127299398e1ac5bb1
musachevaUOC <musacheva@uoc.edu>
2021-02-17 16:11:30 +0100

    working mail sending

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const nodemailer = require("nodemailer");
 const app = express();
 
 const envSecret = process.env.RECAPTCHA_SECRET;
+const senderEmailAdress = process.env.SENDER_EMAIL_ADDRESS;
+const senderEmailPassword = process.env.SENDER_EMAIL_PASSWORD;
+const personalEmailAdress = process.env.PERSONAL_EMAIL_ADDRESS;
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.urlencoded({extended: true}));
@@ -22,12 +27,15 @@ app.post('/', function (req, res) {
     axios.post('https://www.google.com/recaptcha/api/siteverify', data)
      .then(resp=>{
          if(resp.data.success){
-             // TODO: send data to mail
-             console.log("all OK");
-             res.send("all ok");
+             sendDataToEmail(req).then((info)=>{
+                 console.log(info);
+                 res.send("all OK");
+             }).catch((err)=>{
+                console.log(err);
+                res.send("not ok");
+             });
+
          }else{
-             console.log("data not successful");
-             console.log(resp.data);
              res.send("not ok");
          }
      })
@@ -40,10 +48,24 @@ app.post('/', function (req, res) {
 
 app.listen(process.env.PORT || 8080);
 
-function onSuccessResponse(res){
+async function sendDataToEmail(req){
+    let transporter = nodemailer.createTransport({
+        host: "SMTP.Office365.com",
+        port: 587,
+        secure: false,
+        auth:{
+            user: senderEmailAdress,
+            pass: senderEmailPassword
+        },
+    });
 
-}
+    return await transporter.sendMail({
+        from: senderEmailAdress,
+        to: personalEmailAdress,
+        subject: "WebMail: "+req.body['title'],
+        text: "sender: "+req.body['email']+"\n"
+                +"Text: "+"\n"
+                +req.body['text'],
 
-function onErrorResponse(res){
-
+    });
 }
